b1d70151e3a731c672b7e4aedad99dd7e811a274
ibavli <ibrahim.bavli35@gmail.com>
2018-12-09 13:40:46 +0300

    sorted dictionary

---
+++ SortedDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, string> sortedDictionary = new SortedDictionary<int, string>();
+
+            sortedDictionary.Add(2, "2. değer");
+            sortedDictionary.Add(5, "5. değer");
+            sortedDictionary.Add(1, "1. değer");
+            sortedDictionary.Add(4, "4. değer");
+            sortedDictionary.Add(3, "3. değer");
+
+            foreach (var item in sortedDictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
