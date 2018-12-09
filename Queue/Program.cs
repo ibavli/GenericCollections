d312fd69069ffb4a351b58b59ad791bedf15c2f4
ibavli <ibrahim.bavli35@gmail.com>
2018-12-09 12:49:30 +0300

    queue

---
+++ Queue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First in first out => ilk giren ilk çıkar mantığı
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("ali");
+            queue.Enqueue("veli");
+            queue.Enqueue("hasan");
+            queue.Enqueue("hüseyin");
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Peek metodu en üstteki elemanı verir
+            Console.WriteLine("En üstteki eleman => " + queue.Peek());
+
+
+            //En üstteki elemanı siler
+            queue.Dequeue();
+
+            Console.WriteLine("En üstteki eleman => " + queue.Peek());
+
+            Console.ReadLine();
+        }
+    }
+}
